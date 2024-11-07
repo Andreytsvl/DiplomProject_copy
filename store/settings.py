4d9a2664f20a47ff756c69e04d1e9724324e946d
unknown <andreytsvl15@gmail.com>
2024-11-07 14:33:14 +0300

    commit 2

--- store/settings.py
+++ store/settings.py
@@ -21,24 +21,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z(f&uq%od&&c-7h6ap-(zwz^0y6zq_cfi)rvi37nk1v%gn)7%p'
-#SECRET_KEY = os.getenv('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-z(f&uq%od&&c-7h6ap-(zwz^0y6zq_cfi)rvi37nk1v%gn)7%p'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 
-# ALLOWED_HOSTS = [
-# '127.0.0.1',
-# '127.0.0.1:8000',
-# 'andreyts.pythonanywhere.com',
-# 'Andreytsvl.github.io',
-# ]
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'andreyts.pythonanywhere.com',
+    #'Andreytsvl.github.io',
+]
 
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+
 
 # Application definition
 
@@ -98,11 +98,17 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'andreyts$default',
+        'USER': 'andreyts',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'andreyts.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -140,10 +146,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-    ]
-#STATIC_ROOT = BASE_DIR / 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+#     ]
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = 'media/'
 
